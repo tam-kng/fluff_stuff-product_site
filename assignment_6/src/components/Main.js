1d33ac5b153e52c64839953580be1c46bc64b164
Tam Nguyen <tam.k.nguyen14@gmail.com>
2020-10-20 12:20:47 -0400

    refined home page style

--- assignment_6/src/components/Main.js
+++ assignment_6/src/components/Main.js
@@ -10,12 +10,12 @@ function Main() {
                 <h1>Fluff Stuff</h1>
             </div>
             <div className="row">
-                <div className="col-6 promo">SALE: 60% off Cushions</div>
-                <div className="col-6 promo">The coziest you can get.</div>
+                <div className="col-5 promo-left promo-top">SALE: 60% off Cushions</div>
+                <div className="col-5 promo-right promo-top">The coziest you can get.</div>
             </div>
             <div className="row">
-                <div className="col-6 promo">Home and comfort.</div>
-                <div className="col-6 promo">SALE: 20% off Sheets</div>
+                <div className="col-5 promo-left promo-bottom">Home and comfort.</div>
+                <div className="col-5 promo-right promo-bottom">SALE: 20% off Sheets</div>
             </div>
         </main>
     )
