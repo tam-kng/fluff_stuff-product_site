835f01cafd9daa22f30cfc7e883dee8287bd6e10
Tam Nguyen <tam.k.nguyen14@gmail.com>
2020-10-30 17:30:48 -0400

    Updates

---
+++ assignment_6/precache-manifest.afbd5a43b361b95c2124725e2e4e478f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fc172b725838bd5edda6827a95d7b6",
+    "url": "/fluff_stuff-product_site/assignment_6/index.html"
+  },
+  {
+    "revision": "57155f54e4efa24f8218",
+    "url": "/fluff_stuff-product_site/assignment_6/static/css/main.28af22a8.chunk.css"
+  },
+  {
+    "revision": "a144e2bf3e811303b221",
+    "url": "/fluff_stuff-product_site/assignment_6/static/js/2.54813757.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fluff_stuff-product_site/assignment_6/static/js/2.54813757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57155f54e4efa24f8218",
+    "url": "/fluff_stuff-product_site/assignment_6/static/js/main.da130af2.chunk.js"
+  },
+  {
+    "revision": "2ad18b9dbdddfc5a7190",
+    "url": "/fluff_stuff-product_site/assignment_6/static/js/runtime-main.fe4855e4.js"
+  },
+  {
+    "revision": "8533feaeeb78147c3febfc64012d4d56",
+    "url": "/fluff_stuff-product_site/assignment_6/static/media/img_bed_pillow.8533feae.jpg"
+  },
+  {
+    "revision": "6e1bb49a493f87b432dc4f78555e7fe7",
+    "url": "/fluff_stuff-product_site/assignment_6/static/media/img_couch_pillow.6e1bb49a.jpg"
+  },
+  {
+    "revision": "3f91dfad4a0ad5393d928191905d547e",
+    "url": "/fluff_stuff-product_site/assignment_6/static/media/img_floor_pouf.3f91dfad.jpg"
+  },
+  {
+    "revision": "0fd9719b97c5e30d5b7f50bbe33101ea",
+    "url": "/fluff_stuff-product_site/assignment_6/static/media/img_home_banner.0fd9719b.jpg"
+  },
+  {
+    "revision": "c2d3653709deb0557df0e455010053c2",
+    "url": "/fluff_stuff-product_site/assignment_6/static/media/img_round_pillow.c2d36537.jpg"
+  }
+]);
